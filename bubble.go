69097131e3ae2123a161b3d29b6f602e136630f8
mhshajib <shajib0513@gmail.com>
2017-04-23 15:00:34 +0600

    Code comment changed

--- bubble.go
+++ bubble.go
@@ -9,17 +9,17 @@ func main() {
 	fmt.Println("Array Before Sorting : ", a)
 
 	for i:=0; i<6; i++ {
-	  flag := 0;        		//taking a flag variable
+	  flag := 0;//Taking Flag Variable				
 	  for j:=0; j<6-i-1; j++ {
 	  	if a[j] > a[j+1] {
-			temp := a[j];		//
-			a[j] = a[j+1];		//Swapping values
-			a[j+1] = temp;		//
-			flag = 1;			//setting flag as 1, if swapping occurs
+			temp := a[j];
+			a[j] = a[j+1];
+			a[j+1] = temp;
+			flag = 1;//setting flag as 1, if swapping occurs
 		} 
 	  }
-	  if flag == 0 {			//breaking out of for loop to reduce extra loops
-	    break
+	  if flag == 0 {
+	    break//breaking out for loop to reduce extra loops
 	  }
 	}
 	
